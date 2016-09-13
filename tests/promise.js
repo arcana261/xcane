a936de1b75167badf0cf6deab9e1b7bdc2382621
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2016-09-13 16:29:56 +0430

    added promise utilities folder

---
+++ tests/promise.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const promise = require('../lib/promise');
+const expect = require('chai').expect;
+
+function add_node(x, y, cb) {
+  cb(null, x + y);
+}
+
+function do_throw(str, cb) {
+  throw new Error(str);
+}
+
+function do_fail(str, cb) {
+  cb(str);
+}
+
+const self = {data: 5222};
+
+function check_this(cb) {
+  expect(self).to.satisfy(s => s == self);
+  cb();
+}
+
+describe('Utils.Promise', () => {
+  describe('#fromNode()', () => {
+    it('should correctly return result', done => {
+      let p = promise.fromNode(add_node);
+
+      p(5, 12)
+        .then(res => {
+          expect(res).to.equal(17);
+          done();
+        }).catch(done);
+    });
+
+    it('should fail with throw', done => {
+      let p = promise.fromNode(do_throw);
+
+      p('to fail')
+        .then(() => done('it should have failed'))
+        .catch(err => {
+          expect(err.message).to.equal('to fail');
+          done();
+        });
+    });
+
+    it('should fail with reject', done => {
+      let p = promise.fromNode(do_fail);
+
+      p('to fail')
+        .then(() => done('it should have failed'))
+        .catch(err => {
+          expect(err).to.equal('to fail');
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should bind this correctly', done => {
+      let p = promise.fromNode(check_this);
+
+      p.call(self)
+        .then(() => done())
+        .catch(done);
+    });
+  });
+});
