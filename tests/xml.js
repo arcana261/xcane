1a17bf0b39238b90b7e201ebd2885c93c158cdad
Mohamad mehdi Kharatizadeh <m_kharatizadeh@yahoo.com>
2016-09-28 00:58:33 +0330

    added easy XML parser/stringifier module

--- tests/xml.js
+++ tests/xml.js
@@ -10,7 +10,45 @@ describe('XCaneXml', () => {
     });
 
     it('should convert array to XML without options', () => {
-      expect(XML.stringify({a: [{x: 10}, {x: 20}]})).to.be.equal('');
+      expect(XML.stringify({a: [1, 2]})).to.be.equal(
+        '<a>1</a><a>2</a>');
+    });
+
+    it('should convert complex objects without options', () => {
+      expect(XML.stringify({a: [{b: 5}, {c: [6, 7]}]})).to.be.equal(
+        '<a><b>5</b></a><a><c>6</c><c>7</c></a>');
+    });
+  });
+
+  describe('#parse()', () => {
+    it('should parse simple XML without options', () => {
+      expect(XML.parse('<x><a>10</a><b>20</b></x>')).to.be.deep.equal({
+        x: {
+          a: 10,
+          b: 20
+        }
+      });
+    });
+
+    it('should parse array without options', () => {
+      expect(XML.parse('<x><a>1</a><a>2</a></x>')).to.be.deep.equal({
+        x: {
+          a: [1, 2]
+        }
+      });
+    });
+
+    it('should parse complex objects without options', () => {
+      expect(XML.parse('<x><a><b>5</b></a><a><c>6</c><c>7</c></a></x>'))
+        .to.be.deep.equal({
+          x: {
+            a: [{
+              b: 5
+            }, {
+              c: [6, 7]
+            }]
+          }
+        });
     });
   });
 });
